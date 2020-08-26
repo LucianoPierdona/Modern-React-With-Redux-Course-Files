f13b294cf2c03060445f20762b47833113f659d8
Luciano Pierdona <lucianompj1@gmail.com>
2020-08-26 20:32:41 -0300

    Second Update

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 
 const App = () => {
     return (
         <div>
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
                     <div className="ui container">
-                        <Route path="/" exact component={StreamList}/>
-                        <Route path="/streams/create" component={StreamCreate}/>
-                        <Route path="/streams/edit" component={StreamEdit}/>
-                        <Route path="/streams/delete" component={StreamDelete}/>
-                        <Route path="/streams/show" component={StreamShow}/>
+                        <Switch>
+                            <Route path="/" exact component={StreamList}/>
+                            <Route path="/streams/create" exact component={StreamCreate}/>
+                            <Route path="/streams/edit/:id" exact component={StreamEdit}/>
+                            <Route path="/streams/delete/:id" exact component={StreamDelete}/>
+                            <Route path="/streams/:id" exact component={StreamShow}/>
+                        </Switch>
                     </div>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
 
     );
